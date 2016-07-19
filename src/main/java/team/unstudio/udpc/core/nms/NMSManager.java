0ff89b3c7da0c52fb5524eb31d5c3a40bbf0965e
MouseDevelop <937279156@163.com>
2016-07-19 16:46:53 +0800

    Prepared NMS

--- src/main/java/team/unstudio/udpc/core/nms/NMSManager.java
+++ src/main/java/team/unstudio/udpc/core/nms/NMSManager.java
@@ -1,4 +1,9 @@
 package team.unstudio.udpc.core.nms;
 
+import org.bukkit.Bukkit;
+
 public class NMSManager {
+	
+	public static final String NMS_VERSION = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+	
 }
