0756d5162ff3fcfe76c748e2a8eaed2081186ade
MouseDevelop <937279156@163.com>
2016-08-10 12:27:48 +0800

    Added ActionBar

--- src/team/unstudio/udpc/api/nms/NMSPlayer.java
+++ src/team/unstudio/udpc/api/nms/NMSPlayer.java
@@ -7,4 +7,6 @@ public interface NMSPlayer extends NMSEntity{
 	public Player getPlayer();
 	
 	public void sendPacket(Object packet) throws Exception;
+	
+	public void sendActionBar(String message) throws Exception;
 }
