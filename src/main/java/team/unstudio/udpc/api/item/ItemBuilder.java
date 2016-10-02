7fd033b57d8f10f9da2b227878f206d037e78113
MouseDevelop <937279156@163.com>
2016-10-02 10:35:57 +0800

    AreaAPI

--- src/main/java/team/unstudio/udpc/api/item/ItemBuilder.java
+++ src/main/java/team/unstudio/udpc/api/item/ItemBuilder.java
@@ -3,6 +3,7 @@ package team.unstudio.udpc.api.item;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -38,5 +39,28 @@ public class ItemBuilder {
 		meta.setLore(lores);
 		itemStack.setItemMeta(meta);
 		return this;
+	}
+	
+	public ItemBuilder removeLore(int index){
+		ItemMeta meta = itemStack.getItemMeta();
+		ArrayList<String> lores = new ArrayList<>(meta.getLore());
+		lores.remove(index);
+		meta.setLore(lores);
+		itemStack.setItemMeta(meta);
+		return this;
+	}
+	
+	public ItemBuilder addEnchantment(Enchantment ench,int level){
+		itemStack.addUnsafeEnchantment(ench, level);
+		return this;
+	}
+	
+	public ItemBuilder removeEnchantment(Enchantment ench){
+		itemStack.removeEnchantment(ench);
+		return this;
+	}
+	
+	public ItemStack getItemStack(){
+		return itemStack;
 	}
 }
