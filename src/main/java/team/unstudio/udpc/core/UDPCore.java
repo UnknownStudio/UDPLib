50a531acb92e600f4c56e7dfd17722a6ddcfc07d
MouseDevelop <937279156@163.com>
2016-10-02 11:44:09 +0800

    Title

--- src/main/java/team/unstudio/udpc/core/UDPCore.java
+++ src/main/java/team/unstudio/udpc/core/UDPCore.java
@@ -24,7 +24,7 @@ public class UDPCore extends JavaPlugin{
 	
 	public static final String NAME = "UDPCore";
 	public static final String VERSION = "1.0.0-SANPSHOT";
-	//TODO:BossBar,Scoreboard,Hologram,Tab,Title
+	//TODO:BossBar,Scoreboard,Hologram,Tab
 	
 	private static final File PLUGIN_PATH = new File("plugins");
 	
