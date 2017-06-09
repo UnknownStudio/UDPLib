60779fc14aa4b031f19c6a6914e64041d54ef8f0
defoli_ation <defoli_ation@outlook.com>
2017-06-09 16:10:51 +0800

    update

--- src/main/java/team/unstudio/udpl/api/scoreboard/ScoreboardManager.java
+++ src/main/java/team/unstudio/udpl/api/scoreboard/ScoreboardManager.java
@@ -1,10 +1,18 @@
 package team.unstudio.udpl.api.scoreboard;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
 /**
  * 记分板的管理类
  * @author AAA
  *
  */
 public class ScoreboardManager {
-
+	public static void setPlayerScoreboard(Player p,Board board){
+		p.setScoreboard(board.scoreboard);
+	}
+	public static void clear(Player p){
+		p.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
+	}
 }
