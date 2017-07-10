91e9de7763d2a3e827a8821942c7a65773c4b7ce
zMCdarc <zMCdarc@users.noreply.github.com>
2017-07-10 18:53:54 +0800

    Update Board.java

--- src/main/java/team/unstudio/udpl/api/scoreboard/Board.java
+++ src/main/java/team/unstudio/udpl/api/scoreboard/Board.java
@@ -61,69 +61,6 @@ public class Board {
 			map.remove(line);
 		}
 	}
-package team.unstudio.udpl.api.scoreboard;
-
-import java.util.TreeMap;
-
-import org.bukkit.Bukkit;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Score;
-/**
-
- * 
- * @author defoli_ation
- *
- */
-public class Board {
-	protected final org.bukkit.scoreboard.Scoreboard scoreboard;
-	private final Objective objective;
-	private final TreeMap<Integer,String> map = new TreeMap<>();
-	private Score score;
-	/**
-	 * 
-	 * @param title 记分板标题
-	 */
-	public Board(String title){
-		scoreboard=Bukkit.getScoreboardManager().getNewScoreboard();
-		this.objective = scoreboard.registerNewObjective(title, "dummy");
-		this.objective.setDisplayName(title);
-		this.objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-	}
-	/**设置某一行的文本.
-	 * 
-	 * 
-	 *由于记分板最大只能显示16行，所以建议行数为16以内
-	 * @param line 行数
-	 * @param text 文本
-	 */
-	public void set(int line,String text){
-		if(map.containsKey(line))
-			remove(line);
-		map.put(line, text);
-		setup();
-	}
-	/**
-	 * 获得记分板上这一行的文本.
-	 * 若为空则返回null
-	 * @param line 行数
-	 * @return String
-	 */
-	public String get(int line){
-		return map.get(line);
-	}
-	/**
-	 * 移除对应行数的文本
-	 * @param line 某一行
-	 * 
-	 */
-	public void remove(int line){
-		if(line>=16)return;
-		if(map.containsKey(line)){
-			this.scoreboard.resetScores(map.get(line));
-			map.remove(line);
-		}
-	}
 
 	/**
 	 * 移除对应的文本
