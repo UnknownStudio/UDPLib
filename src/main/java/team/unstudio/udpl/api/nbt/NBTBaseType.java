54f46581cdd0e7dcfa3d1059abea940dbea4fc52
Mcdarc <mcdarc@163.com>
2017-07-10 18:23:04 +0800

    还没有完成对Array、List的解析

--- src/main/java/team/unstudio/udpl/api/nbt/NBTBaseType.java
+++ src/main/java/team/unstudio/udpl/api/nbt/NBTBaseType.java
@@ -1,28 +1,34 @@
 package team.unstudio.udpl.api.nbt;
 
 public enum NBTBaseType {
-	STRING(8),
-	BYTE(1),
-	DOUBLE(6),
-	FLOAT(5),
-	INTEGER(3),
-	INTARRAY(11),
-	BYTEARRAY(7),
-	COMPOUND(10),
-	LIST(9),
-	LONG(4),
-	SHORT(2),
-	BOOLEAN(-1), // do you sure it has a NBT Tag named BOOLEAN?  see http://minecraft-zh.gamepedia.com/NBT%E6%A0%BC%E5%BC%8F
-	END(0);
+	STRING(8, ""),
+	BYTE(1, "b"),
+	DOUBLE(6, "d"),
+	FLOAT(5, "f"),
+	INTEGER(3, ""),
+	INTARRAY(11, "ints"), // 不确定
+	BYTEARRAY(7, "bytes"),
+	COMPOUND(10, ""), // 不确定
+	LIST(9, ""), // 不确定
+	LONG(4, "L"),
+	SHORT(2, "s"),
+	BOOLEAN(-1, "b"), // 0 is false 1 is true
+	END(0, "");
 
 	private int id;
+	private String sign;
 
-    private NBTBaseType(int i) {
+    private NBTBaseType(int i, String sign) {
         this.id = i;
+        this.sign = sign;
     }
 
     public int getID() {
-	    return id;
+	    return this.id;
+    }
+
+    public String getSign() {
+        return this.sign;
     }
 
     @Override
