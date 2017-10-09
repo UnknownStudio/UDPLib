0db6db745083e60d9df828333336ce01a78a55c0
Mouse <937279156@qq.com>
2017-10-09 13:07:49 +0800

    Update ItemWrapper

--- src/main/java/team/unstudio/udpl/item/ItemWrapper.java
+++ src/main/java/team/unstudio/udpl/item/ItemWrapper.java
@@ -1,15 +1,18 @@
 package team.unstudio.udpl.item;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Map;
+import java.util.Set;
 
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import com.google.common.collect.Lists;
 
 public class ItemWrapper {
 
@@ -34,20 +37,37 @@ public class ItemWrapper {
 	public ItemWrapper(ItemStack itemStack) {
 		this.itemStack = itemStack;
 	}
+	
+	public Material getType(){
+		return itemStack.getType();
+	}
 
 	public ItemWrapper setType(Material type){
 		itemStack.setType(type);
 		return this;
+	}
+	
+	public short getDurability(){
+		return itemStack.getDurability();
 	}
 
 	public ItemWrapper setDurability(short durability){
 		itemStack.setDurability(durability);
 		return this;
 	}
+	
+	public int getAmount(){
+		return itemStack.getAmount();
+	}
 
 	public ItemWrapper setAmount(int amount){
 		itemStack.setAmount(amount);
 		return this;
+	}
+	
+	public String getDisplayName(){
+		ItemMeta meta = itemStack.getItemMeta();
+		return meta.hasDisplayName()?meta.getDisplayName():"";
 	}
 
 	public ItemWrapper setDisplayName(String name){
@@ -56,6 +76,11 @@ public class ItemWrapper {
 		itemStack.setItemMeta(meta);
 		return this;
 	}
+	
+	public String getLocalizedName(){
+		ItemMeta meta = itemStack.getItemMeta();
+		return meta.hasLocalizedName()?meta.getLocalizedName():"";
+	}
 
 	public ItemWrapper setLocalizedName(String name){
 		ItemMeta meta = itemStack.getItemMeta();
@@ -63,62 +88,74 @@ public class ItemWrapper {
 		itemStack.setItemMeta(meta);
 		return this;
 	}
+	
+	public List<String> getLore(){
+		ItemMeta meta = itemStack.getItemMeta();
+		if(meta.hasLore())
+			return meta.getLore();
+		else
+			return Lists.newArrayList();
+	}
+	
+	public ItemWrapper setLore(String... lore){
+		setLore(Arrays.asList(lore));
+		return this;
+	}
+	
+	public ItemWrapper setLore(List<String> lore){
+		itemStack.getItemMeta().setLore(lore);
+		return this;
+	}
 
-	public ItemWrapper addLore(String ...lore){
-		List<String> lores =  getLore(itemStack);
+	public ItemWrapper addLore(String... lore){
+		List<String> lores = getLore();
 		Collections.addAll(lores, lore);
-		setMeta(lores);
+		setLore(lores);
 		return this;
 	}
 	
-	public ItemWrapper addLore(int index,String ...lore){
-		List<String> lores =  getLore(itemStack);
-		lores.addAll(index, getList(lore));
-		setMeta(lores);
+	public ItemWrapper addLore(int index, String... lore){
+		List<String> lores = getLore();
+		lores.addAll(index, Arrays.asList(lore));
+		setLore(lores);
 		return this;
-	}
-	
-	private List<String> getList(String[] lore){
-		ArrayList<String> list = new ArrayList<>();
-		for(String s : lore) {
-			list.add(s);
-		}
-		return list;
 	}
 
 	public ItemWrapper removeLore(int index){
-		List<String> lores =  getLore(itemStack);
+		List<String> lores = getLore();
 		lores.remove(index);
-		setMeta(lores);
+		setLore(lores);
 		return this;
 	}
 
 	public ItemWrapper removeLore(String regex){
-		List<String> lores =  getLore(itemStack);
+		List<String> lores = getLore();
 		for(String s : lores) {
 			if(s.matches(regex)) 
 				lores.remove(s);
 		}
-		setMeta(lores);
+		setLore(lores);
 		return this;
 	}
-
-	private List<String> getLore(ItemStack item){
-		CopyOnWriteArrayList<String> lore = new CopyOnWriteArrayList<String>();
-		if(!item.hasItemMeta())return lore;
-		ItemMeta meta = item.getItemMeta();
-		if(meta.hasLore()&&!meta.getLore().isEmpty())
-			lore.addAll(meta.getLore());
-		return lore;
+	
+	public Map<Enchantment, Integer> getEnchantments(){
+		return itemStack.getEnchantments();
 	}
-
-	private void setMeta(List<String> lore) {
-		ItemMeta meta = itemStack.getItemMeta();
-		meta.setLore(lore);
-		itemStack.setItemMeta(meta);
+	
+	public boolean hasEnchantment(Enchantment ench){
+		return itemStack.containsEnchantment(ench);
+	}
+	
+	public int getEnchantmentLevel(Enchantment ench){
+		return itemStack.getEnchantmentLevel(ench);
 	}
 
 	public ItemWrapper addEnchantment(Enchantment ench,int level){
+		itemStack.addEnchantment(ench, level);
+		return this;
+	}
+	
+	public ItemWrapper addUnsafeEnchantment(Enchantment ench,int level){
 		itemStack.addUnsafeEnchantment(ench, level);
 		return this;
 	}
@@ -127,12 +164,31 @@ public class ItemWrapper {
 		itemStack.removeEnchantment(ench);
 		return this;
 	}
+	
+	public Set<ItemFlag> getItemFlags(){
+		return itemStack.getItemMeta().getItemFlags();
+	}
+	
+	public boolean hasItemFlag(ItemFlag flag){
+		return itemStack.getItemMeta().hasItemFlag(flag);
+	}
 
-	public ItemWrapper addFlag(ItemFlag... flags) {
+	public ItemWrapper addItemFlags(ItemFlag... flags) {
 		ItemMeta meta = itemStack.getItemMeta();
 		meta.addItemFlags(flags);
 		itemStack.setItemMeta(meta);
 		return this;
+	}
+	
+	public ItemWrapper removeItemFlags(ItemFlag... flags) {
+		ItemMeta meta = itemStack.getItemMeta();
+		meta.removeItemFlags(flags);
+		itemStack.setItemMeta(meta);
+		return this;
+	}
+	
+	public boolean isUnbreakable(){
+		return itemStack.getItemMeta().isUnbreakable();
 	}
 
 	public ItemWrapper setUnbreakable(boolean value){
