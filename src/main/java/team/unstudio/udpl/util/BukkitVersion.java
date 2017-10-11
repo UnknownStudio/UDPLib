6d0dde33520de615cba3fda4d278eb9d15b4c30c
Mouse <937279156@qq.com>
2017-10-11 17:30:51 +0800

    Update BukkitVersion

--- src/main/java/team/unstudio/udpl/util/BukkitVersion.java
+++ src/main/java/team/unstudio/udpl/util/BukkitVersion.java
@@ -4,24 +4,22 @@ import team.unstudio.udpl.nms.ReflectionUtils;
 
 public enum BukkitVersion{
 	
-	V1_12_R1(1201),
-	V1_11_R1(1101),
-	V1_10_R1(1001),
-	V1_9_R2(902),
-	V1_9_R1(901),
-	V1_8_R3(803),
-	V1_8_R2(802),
-	V1_8_R1(801);
+	V1_12_R1,
+	V1_11_R1,
+	V1_10_R1,
+	V1_9_R2,
+	V1_9_R1,
+	V1_8_R3,
+	V1_8_R2,
+	V1_8_R1;
 
 	public static final BukkitVersion CURRENT_BUKKIT_VERSION = valueOf(ReflectionUtils.PackageType.getServerVersion().toUpperCase());
 			
-	private final int index;
-	
-	BukkitVersion(int index) {
-		this.index = index;
+	public boolean isAbove(BukkitVersion value){
+		return compareTo(value) >= 0;
 	}
-
-	public int getIndex() {
-		return index;
+	
+	public boolean isBelow(BukkitVersion value){
+		return compareTo(value) <= 0;
 	}
 }
