bb780eb0641bd08a05037c5e3d7f7f5b6ac4d179
Mouse <937279156@qq.com>
2017-10-15 12:46:07 +0800

    Update TestCommand

--- src/main/java/team/unstudio/udpl/core/test/TestCommand.java
+++ src/main/java/team/unstudio/udpl/core/test/TestCommand.java
@@ -1,62 +1,24 @@
 package team.unstudio.udpl.core.test;
 
-import org.bukkit.Material;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-
 import team.unstudio.udpl.command.anno.Alias;
 import team.unstudio.udpl.command.anno.Command;
 import team.unstudio.udpl.command.anno.Optional;
 import team.unstudio.udpl.command.anno.Required;
-import team.unstudio.udpl.ui.UI;
-import team.unstudio.udpl.ui.UIFactory;
+import team.unstudio.udpl.util.Title;
 
 public final class TestCommand {
-
-	UI ui;
-	Player player;
-	
-	@Command(value = "opengui", 
-			senders = Player.class)
-	public void openGUI(Player sender) {
-		ui = UIFactory.createUI(9, "TestUI");
-		ui.open(sender);
-		player = sender;
-	}
-	
-	@Command(value = "closegui", 
-			senders = ConsoleCommandSender.class)
-	public void closeGUI(ConsoleCommandSender sender) {
-		ui.close(player);
-	}
-	
-	@Command(value = "give",senders = Player.class)
-	public boolean giveItemToPlayer(CommandSender sender,@Required Player target,@Required Material itemType,@Optional("1") int amount,@Optional("0") short durability){
-		if(target==null)
-			return false;
-		
-		if(itemType==null)
-			return false;
-		
-		ItemStack item = new ItemStack(itemType);
-		item.setAmount(amount);
-		item.setDurability(durability);
-		
-		target.getInventory().addItem(item);
-		return true;
-	}
 	
 	@Command(senders = Player.class)
-	@Alias({"tpa"})
-	@Alias({"teleport"})
-	public boolean tpa(Player sender,@Required Player target){
-		if(target==null||!target.isOnline())
-			return false;
-		
-		sender.teleport(target);
-		
+	@Alias({"help"})
+	public boolean help(Player sender){
+		sender.sendMessage("UDPL Test 已正常启动.");
 		return true;
+	}
+
+	@Command(value = { "title" }, senders = Player.class)
+	public void title(Player sender, @Required String title, @Required String subTitle, @Optional("5") int fadeIn,
+			@Optional("10") int stay, @Optional("5") int fadeOut) {
+		Title.title(sender, title, subTitle, fadeIn, stay, fadeOut);
 	}
 }
