fbabe43b60c981c72393d2818ffc08f442507ccf
Mouse <937279156@qq.com>
2017-10-15 12:39:52 +0800

    Update ServerUtils

--- src/main/java/team/unstudio/udpl/util/ServerUtils.java
+++ src/main/java/team/unstudio/udpl/util/ServerUtils.java
@@ -1,5 +1,6 @@
 package team.unstudio.udpl.util;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.function.Predicate;
 
 import org.bukkit.Bukkit;
@@ -16,7 +17,20 @@ public final class ServerUtils {
 		return Bukkit.getOnlinePlayers().stream().map(player->player.getName()).filter(filter).toArray(String[]::new);
 	}
 	
-	public static String getMinecraftVersion(){
-		return Bukkit.getBukkitVersion().substring(0, Bukkit.getBukkitVersion().indexOf("-"));
+	private static String MINECRAFT_VERSION = null;
+
+	public static String getMinecraftVersion() {
+		if (MINECRAFT_VERSION == null) {
+			try {
+				MINECRAFT_VERSION = (String) ReflectionUtils
+						.invokeMethod(ReflectionUtils.getValue(Bukkit.getServer(), true, "console"), "getVersion");
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException
+					| NoSuchMethodException | NoSuchFieldException | SecurityException e) {
+				String bukkitVersion = Bukkit.getBukkitVersion();
+				int index = Bukkit.getBukkitVersion().indexOf("-");
+				MINECRAFT_VERSION = bukkitVersion.substring(0, index == -1 ? bukkitVersion.length() : index);
+			}
+		}
+		return MINECRAFT_VERSION;
 	}
 }
