b6573f71ea699f67423b0a7e7876684e252e7868
Mouse <937279156@qq.com>
2017-10-16 23:51:24 +0800

    Update BlockUtils

--- src/main/java/team/unstudio/udpl/util/BlockUtils.java
+++ src/main/java/team/unstudio/udpl/util/BlockUtils.java
@@ -27,8 +27,21 @@ public final class BlockUtils {
 	 * @return
 	 */
 	public static Result sendBlockBreakAnimation(Player player, Location location, byte state) {
+		return sendBlockBreakAnimation(player, player.getEntityId(), location, state);
+	}
+	
+	/**
+	 * 0–9 are the displayable destroy stages and each other number means that there is no animation on this coordinate.</br>
+	 * Block break animations can still be applied on air; the animation will remain visible although there is no block being broken. However, if this is applied to a transparent block, odd graphical effects may happen, including water losing its transparency. (An effect similar to this can be seen in normal gameplay when breaking ice blocks)</br>
+	 * If you need to display several break animations at the same time you have to give each of them a unique Entity ID.</br>
+	 * @param player
+	 * @param location
+	 * @param state
+	 * @return
+	 */
+	public static Result sendBlockBreakAnimation(Player player, int entityId, Location location, byte state) {
 		PacketContainer container = protocolManager.createPacket(PacketType.Play.Server.BLOCK_BREAK_ANIMATION);
-		container.getIntegers().write(0, player.getEntityId());
+		container.getIntegers().write(0, entityId);
 		container.getBlockPositionModifier().write(0,
 				new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ()));
 		container.getIntegers().write(1, (int) state);
