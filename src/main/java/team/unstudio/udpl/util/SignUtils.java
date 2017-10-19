5b5f0830ecd674fea5d9620f1e7c41fcce77fb1c
Mouse <937279156@qq.com>
2017-10-19 13:15:15 +0800

    Update SignEvent

--- src/main/java/team/unstudio/udpl/util/SignUtils.java
+++ src/main/java/team/unstudio/udpl/util/SignUtils.java
@@ -3,15 +3,26 @@ package team.unstudio.udpl.util;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListeningWhitelist;
 import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.events.PacketListener;
 import com.comphenix.protocol.wrappers.BlockPosition;
 import com.comphenix.protocol.wrappers.WrappedBlockData;
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import com.comphenix.protocol.wrappers.nbt.NbtCompound;
+import com.google.common.collect.Sets;
 
+import team.unstudio.udpl.core.UDPLib;
+import team.unstudio.udpl.event.FakeSignUpdateEvent;
+
+import java.util.Set;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 /**
  * Created by trychen on 17/7/11.
@@ -24,8 +35,51 @@ public final class SignUtils {
     private static PacketType UPDATE_SIGN = PacketType.Play.Server.TILE_ENTITY_DATA;
     private static PacketType OPEN_SIGN_ENTITY = PacketType.Play.Server.OPEN_SIGN_EDITOR;
     
+    private static final Set<Player> OPENED_FAKE_SIGN_PLAYERS = Sets.newHashSet();
+    
     private SignUtils(){}
+    
+    public static void initSignUtils(){
+    	manager.addPacketListener(new PacketListener() {
+			
+			@Override
+			public void onPacketSending(PacketEvent arg0) {}
+			
+			@Override
+			public void onPacketReceiving(PacketEvent arg0) {
+				Player player = arg0.getPlayer();
+				if(!OPENED_FAKE_SIGN_PLAYERS.contains(player))
+					return;
+				PacketContainer container = arg0.getPacket();
+				String line1 = container.getStrings().read(0);
+				String line2 = container.getStrings().read(1);
+				String line3 = container.getStrings().read(2);
+				String line4 = container.getStrings().read(3);
+				Bukkit.getPluginManager()
+						.callEvent(new FakeSignUpdateEvent(player, new String[] { line1, line2, line3, line4 }));
+				OPENED_FAKE_SIGN_PLAYERS.remove(player);
+			}
 
+			@Override
+			public ListeningWhitelist getSendingWhitelist() {
+				return ListeningWhitelist.EMPTY_WHITELIST;
+			}
+			
+			@Override
+			public ListeningWhitelist getReceivingWhitelist() {
+				return ListeningWhitelist.newBuilder().lowest().types(PacketType.Play.Client.UPDATE_SIGN).build();
+			}
+			
+			@Override
+			public Plugin getPlugin() {
+				return UDPLib.getInstance();
+			}
+		});
+    }
+
+    /**
+     * If you want to receive sign update, to see {@link team.unstudio.udpl.event.FakeSignUpdateEvent}
+     */
     public static Result open(Player player, String[] lines){
         Location loc = player.getLocation();
         int x = loc.getBlockX();
@@ -46,6 +100,7 @@ public final class SignUtils {
                 e = manager.createPacket(OPEN_SIGN_ENTITY);
                 e.getBlockPositionModifier().write(0, blockPosition);
                 manager.sendServerPacket(player, e);
+                OPENED_FAKE_SIGN_PLAYERS.add(player);
                 return Result.success();
             } catch (Exception var11) {
             	return Result.failure(var11);
@@ -63,6 +118,7 @@ public final class SignUtils {
                 e = manager.createPacket(OPEN_SIGN_ENTITY);
                 e.getBlockPositionModifier().write(0, blockPosition);
                 manager.sendServerPacket(player, e);
+                OPENED_FAKE_SIGN_PLAYERS.add(player);
                 return Result.success();
             } catch (Exception var12) {
             	return Result.failure(var12);
@@ -85,6 +141,7 @@ public final class SignUtils {
                 e = manager.createPacket(OPEN_SIGN_ENTITY);
                 e.getBlockPositionModifier().write(0, blockPosition);
                 manager.sendServerPacket(player, e);
+                OPENED_FAKE_SIGN_PLAYERS.add(player);
                 return Result.success();
             } catch (Exception var12) {
             	return Result.failure(var12);
