7131e0e17bc42cd1797a72f512f04b038c384cca
Mouse <937279156@qq.com>
2017-11-03 18:06:16 +0800

    Update Nms

--- src/main/java/team/unstudio/udpl/nms/NmsHelper.java
+++ src/main/java/team/unstudio/udpl/nms/NmsHelper.java
@@ -14,16 +14,39 @@ public final class NmsHelper {
 	
 	private static AsmNmsManager ASM_NMS_MANAGER;
 
-	public static void loadNmsHelper(){
-		ASM_NMS_MANAGER = new AsmNmsManager();
-		UDPLib.getLog().info("Loaded nms helper. Version: " + ReflectionUtils.PackageType.getServerVersion());
-	}
-	
+	/**
+	 * 获取 {@link net.minecraft.server} 与  {@link team.unstudio.udpl.nms.nbt} 的帮助类实例
+	 * @return
+	 */
 	public static NmsNBT getNBT(){
 		return ASM_NMS_MANAGER.getNmsNBT();
 	}
 	
+	
+	/**
+	 * 创建一个NmsItemStack对象
+	 * @param itemStack
+	 * @return
+	 * @deprecated {@link NmsHelper#createNmsItemStack(ItemStack)}
+	 */
 	public static NmsItemStack createItemStack(ItemStack itemStack){
 		return ASM_NMS_MANAGER.createNmsItemStack(itemStack);
+	}
+	
+	/**
+	 * 创建一个NmsItemStack对象
+	 * @param itemStack
+	 * @return
+	 */
+	public static NmsItemStack createNmsItemStack(ItemStack itemStack){
+		return ASM_NMS_MANAGER.createNmsItemStack(itemStack);
+	}
+	
+	/**
+	 * inner method
+	 */
+	public static void loadNmsHelper(){
+		ASM_NMS_MANAGER = new AsmNmsManager();
+		UDPLib.getLog().info("Loaded nms helper. Version: " + ReflectionUtils.PackageType.getServerVersion());
 	}
 }
