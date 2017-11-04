3ab071bd184ee11ff7614b4fe924f19ffd3c125b
Mouse <937279156@qq.com>
2017-11-04 22:43:28 +0800

    Update example

--- example/team/unstudio/udpl/example/ExampleConfig.java
+++ example/team/unstudio/udpl/example/ExampleConfig.java
@@ -3,14 +3,13 @@ package team.unstudio.udpl.example;
 import java.io.File;
 
 import team.unstudio.udpl.config.ConfigurationHandler;
-import team.unstudio.udpl.config.serialization.Setting;
 
 public class ExampleConfig extends ConfigurationHandler{
 	
-	@Setting("text") // 配置文件的键
+	@ConfigItem("text") // 配置文件的键
 	public String example;
 	
-	// 如果没有@Setting注解，就将字段名作为配置文件的键
+	// 如果没有@ConfigItem注解，就将字段名作为配置文件的键
 	public boolean enableExample = true; // 设置字段的默认值为true
 
 	public ExampleConfig(File file) {
