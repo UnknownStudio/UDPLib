937a29f0c8d05c69cf05aa838337025491001a16
Mouse <937279156@qq.com>
2017-11-05 16:36:59 +0800

    Update BukkitVersion

--- src/main/java/team/unstudio/udpl/util/BukkitVersion.java
+++ src/main/java/team/unstudio/udpl/util/BukkitVersion.java
@@ -4,15 +4,24 @@ package team.unstudio.udpl.util;
  * Bukkit version checker
  */
 public enum BukkitVersion{
-	V1_12_R1,
-	V1_11_R1,
-	V1_10_R1,
-	V1_9_R2,
-	V1_9_R1,
-	V1_8_R3,
-	V1_8_R2,
-	V1_8_R1,
-	UNKNOWN;
+	
+	V1_12_R1("v1_12_R1","1.12.2"),
+	V1_11_R1("v1_11_R1","1.11.2"),
+	V1_10_R1("v1_10_R1","1.10.2"),
+	V1_9_R2("v1_9_R2","1.9.4"),
+	V1_9_R1("v1_9_R1","1.9"),
+	V1_8_R3("v1_8_R3","1.8.7"),
+	V1_8_R2("v1_8_R2","1.8.3"),
+	V1_8_R1("v1_8_R1","1.8"),
+	UNKNOWN(ReflectionUtils.PackageType.getServerVersion(),ServerUtils.getMinecraftVersion());
+	
+	private final String packetName;
+	private final String lastMinecraftVersion;
+	
+	BukkitVersion(String packetName,String lastMinecraftVersion) {
+		this.packetName = packetName;
+		this.lastMinecraftVersion = lastMinecraftVersion;
+	}
 
 	/**
 	 * if version equal or newer than the version you gave
@@ -41,7 +50,11 @@ public enum BukkitVersion{
 	 * get package name like "v1_12_R1"
 	 */
 	public String getPackageName(){
-		return 'v'+(name().substring(1));
+		return packetName;
+	}
+	
+	public String getLastMinecraftVersion(){
+		return lastMinecraftVersion;
 	}
 
 	private static BukkitVersion CURRENT_BUKKIT_VERSION;
