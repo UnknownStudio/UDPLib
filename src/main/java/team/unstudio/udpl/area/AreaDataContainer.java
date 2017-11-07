4d1eadc8cde4ce43db4f362f213726b7deb8775c
Mouse <937279156@qq.com>
2017-11-07 23:35:12 +0800

    Update Area

--- src/main/java/team/unstudio/udpl/area/AreaDataContainer.java
+++ src/main/java/team/unstudio/udpl/area/AreaDataContainer.java
@@ -4,7 +4,6 @@ import org.bukkit.configuration.serialization.ConfigurationSerializable;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 public class AreaDataContainer extends HashMap<String, Object> implements ConfigurationSerializable{
 	
@@ -32,18 +31,6 @@ public class AreaDataContainer extends HashMap<String, Object> implements Config
 		}catch(ClassCastException e){
 			return def;
 		}
-	}
-	
-	@Override
-	public Object put(String key, Object value){
-		if(!checkKey(key))
-			throw new IllegalArgumentException("Wrong key pattern");
-		return super.put(key, value);
-	}
-	
-	private static final Pattern KEY_PATTERN = Pattern.compile("[A-Z|a-z|0-9|_|$]+");
-	private boolean checkKey(String key){
-		return KEY_PATTERN.matcher(key).matches();
 	}
 
 	@Override
