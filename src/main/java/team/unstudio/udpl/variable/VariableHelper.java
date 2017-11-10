ee5942ec9b1fa03a1d129d125fdaa5388e08aa9e
Mouse <937279156@qq.com>
2017-11-11 00:30:42 +0800

    Update Variable API

--- src/main/java/team/unstudio/udpl/variable/VariableHelper.java
+++ src/main/java/team/unstudio/udpl/variable/VariableHelper.java
@@ -1,8 +1,6 @@
 package team.unstudio.udpl.variable;
 
-import java.util.Map;
-import java.util.function.Function;
-
+import java.util.Set;
 import javax.annotation.Nonnull;
 
 import org.apache.commons.lang.Validate;
@@ -10,58 +8,57 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import com.google.common.base.Strings;
-import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 public final class VariableHelper {
 
 	private VariableHelper(){}
 	
-	private static final Map<String,Variable> REGISTED_VARIABLES = Maps.newHashMap();
+	private static final Set<VariableHandler> REGISTED_VARIABLE_HANDLERS = Sets.newLinkedHashSet();
 	
-	public static void registerVariable(@Nonnull Variable variable){
-		Validate.notNull(variable);
-		REGISTED_VARIABLES.put(variable.getName(), variable);
-	}
-
-	public static SimpleVariable registerVariable(@Nonnull Plugin plugin, @Nonnull String name, @Nonnull Function<Player, String> function){
-		SimpleVariable variable = new SimpleVariable(plugin, name, function);
-		registerVariable(variable);
-		return variable;
+	public static void registerVariable(@Nonnull VariableHandler handler){
+		Validate.notNull(handler);
+		REGISTED_VARIABLE_HANDLERS.add(handler);
 	}
 	
-	public static void unregisterVariable(@Nonnull Variable variable){
-		Validate.notNull(variable);
-		REGISTED_VARIABLES.remove(variable.getName());
+	public static void unregisterVariable(@Nonnull VariableHandler handler){
+		Validate.notNull(handler);
+		REGISTED_VARIABLE_HANDLERS.remove(handler.getName());
 	}
 	
-	public static void unregisterVariable(@Nonnull String variable){
-		Validate.notEmpty(variable);
-		REGISTED_VARIABLES.remove(variable);
+	public static void unregisterVariable(@Nonnull String name){
+		Validate.notEmpty(name);
+		REGISTED_VARIABLE_HANDLERS.removeIf(handler->name.equals(handler.getName()));
+	}
+	
+	public static void unregisterVariable(@Nonnull Plugin plugin){
+		Validate.notNull(plugin);
+		REGISTED_VARIABLE_HANDLERS.removeIf(handler->plugin.equals(handler.getPlugin()));
 	}
 	
 	public static String apply(Player player, String value){
 		return apply(player, value, '%', '%');
 	}
 	
-	public static String apply(Player player, String value, char variableStart, char variableEnd){
+	public static String apply(Player player, String value, char handlerStart, char handlerEnd){
 		StringBuilder result = new StringBuilder(value.length());
-		StringBuilder variableName = new StringBuilder();
-		boolean variableMatchesing = false;
+		StringBuilder handlerName = new StringBuilder();
+		boolean handlerMatchesing = false;
 		for(char c:value.toCharArray()){
-			if(variableMatchesing){
-				if(c == variableEnd){
-					variableMatchesing = false;
-					String variable = getValue(player, variableName.toString());
-					if(Strings.isNullOrEmpty(variable))
-						result.append(variableStart).append(variableName.toString()).append(variableEnd);
+			if(handlerMatchesing){
+				if(c == handlerEnd){
+					handlerMatchesing = false;
+					String handler = getValue(player, handlerName.toString());
+					if(Strings.isNullOrEmpty(handler))
+						result.append(handlerStart).append(handlerName.toString()).append(handlerEnd);
 					else
-						result.append(variable);
-					variableName.delete(0, variableName.length());
+						result.append(handler);
+					handlerName.delete(0, handlerName.length());
 				}else
-					variableName.append(c);
+					handlerName.append(c);
 			}else{
-				if(c == variableStart)
-					variableMatchesing = true;
+				if(c == handlerStart)
+					handlerMatchesing = true;
 				else
 					result.append(c);
 			}
@@ -69,10 +66,14 @@ public final class VariableHelper {
 		return result.toString();
 	}
 	
-	private static String getValue(Player player,String name){
-		if(REGISTED_VARIABLES.containsKey(name))
-			return REGISTED_VARIABLES.get(name).getValue(player);
-		else
-			return null;
+	private static String getValue(Player player, String name){
+		for(VariableHandler handler : REGISTED_VARIABLE_HANDLERS){
+			String value = handler.get(player, name);
+			if(value == null)
+				continue;
+			
+			return value;
+		}
+		return null;
 	}
 }
