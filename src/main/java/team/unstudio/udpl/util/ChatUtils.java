2c53717151516de8fbecfdc2362237c8116ebfee
Mouse <937279156@qq.com>
2017-11-17 00:06:52 +0800

    Update ChatUtils

--- src/main/java/team/unstudio/udpl/util/ChatUtils.java
+++ src/main/java/team/unstudio/udpl/util/ChatUtils.java
@@ -5,17 +5,26 @@ import org.bukkit.entity.Player;
 
 public interface ChatUtils {
 
-	static String SPLITTER = ChatColor.translateAlternateColorCodes('&', "&l&m----------------------------------------------------------------");
+	static String SPLITTER = translateColorCodes("&l&m----------------------------------------------------------------");
 	static char DEFAULT_COLOR_CHAR = '&';
 	
+	/**
+	 * 发送分割线
+	 */
 	static void sendSplitter(Player player){
 		player.sendMessage(SPLITTER);
 	}
 	
+	/**
+	 * 发送空行
+	 */
 	static void sendEmpty(Player player){
 		player.sendMessage("");
 	}
 	
+	/**
+	 * 将文本中'&'转换为颜色字符
+	 */
 	static String translateColorCodes(String textToTranslate){
 		return ChatColor.translateAlternateColorCodes(DEFAULT_COLOR_CHAR, textToTranslate);
 	}
