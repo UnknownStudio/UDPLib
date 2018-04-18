f9430bfd24f42d0a75519d47d40db077ade50a27
Mouse <937279156@qq.com>
2018-04-19 00:11:50 +0800

    Update ASMClassLoader

--- udplib-common/src/main/java/team/unstudio/udpl/util/extra/ASMClassLoader.java
+++ udplib-common/src/main/java/team/unstudio/udpl/util/extra/ASMClassLoader.java
@@ -1,19 +1,33 @@
 package team.unstudio.udpl.util.extra;
 
-public class ASMClassLoader extends ClassLoader{
+public class ASMClassLoader extends ClassLoader {
 
 	public ASMClassLoader() {
 	}
-	
+
 	public ASMClassLoader(ClassLoader parent) {
 		super(parent);
 	}
-	
+
 	public Class<?> loadClass(byte[] b) {
-		return loadClass(null, b, 0, b.length);
+		return loadClass(null, b);
 	}
-	
+
+	public Class<?> loadClass(String name, byte[] b) {
+		return defineClass(name, b, 0, b.length);
+	}
+
 	public Class<?> loadClass(String name, byte[] b, int off, int len) {
-		return defineClass(name, b, off, len);
+		Class<?> clazz = defineClass(name, b, off, len);
+		return clazz;
+	}
+
+	public boolean hasClass(String name) {
+		try {
+			Class.forName(name);
+			return true;
+		} catch (ClassNotFoundException e) {
+			return false;
+		}
 	}
 }
